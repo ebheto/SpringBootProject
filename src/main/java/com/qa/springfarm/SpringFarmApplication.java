53848e57b6c6ebb6dbbb175c80c21f0036bd7f75
ebheto <ebhetome@protonmail.com>
2022-02-08 09:59:14 +0000

    added files

---
+++ src/main/java/com/qa/springfarm/SpringFarmApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/springfarm/SpringFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.springfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
